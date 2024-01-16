6164facebba77508e21ecd6fdb657c9f5a5b1fa9
shio <shion48691811@gmail.com>
2024-01-16 20:38:00 +0900

    記事機能を作成

--- main.py
+++ main.py
@@ -6,5 +6,5 @@ app = FastAPI()
 @app.get("/search")
 def read_root():
     return {"page": "Search Page"}
-
+app.mount("/img-data", StaticFiles(directory="img-data",html=True), name="static")
 app.mount("/", StaticFiles(directory="static",html=True), name="static")

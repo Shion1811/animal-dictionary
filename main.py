0149bc8c6b92f47bd981b23c2d365d3db8302771
shio <shion48691811@gmail.com>
2024-02-06 20:34:54 +0900

    単語ページを動的に変更

--- main.py
+++ main.py
@@ -1,9 +1,20 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
 
 from utils import StatiFilesNoCache
 
 
 app = FastAPI()
+
+templates = Jinja2Templates(directory="templates")
+
+@app.get("/words/{word}", response_class=HTMLResponse)
+async def read_word(request: Request, word: str):
+    return templates.TemplateResponse(
+        request=request, name="word.html", context={"word": word}
+    )
 
 # サーチページのサンプル
 @app.get("/search")

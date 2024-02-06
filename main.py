39d52c58e20f59cf2f3b2cb68db4a1dfe550df74
shio <shion48691811@gmail.com>
2024-02-06 20:20:53 +0900

    正規ファイルのマウントのタイミングを変更

--- main.py
+++ main.py
@@ -5,11 +5,11 @@ from utils import StatiFilesNoCache
 
 app = FastAPI()
 
-# 静的ファイルを設定
-app.mount("/", StatiFilesNoCache(directory="static",html=True), name="static")
-app.mount("/img-data", StatiFilesNoCache(directory="img-data",html=True), name="static")
-
 # サーチページのサンプル
 @app.get("/search")
 def read_root():
     return {"page": "Search Page"}
+
+# 静的ファイルを設定
+app.mount("/", StatiFilesNoCache(directory="static",html=True), name="static")
+app.mount("/img-data", StatiFilesNoCache(directory="img-data",html=True), name="static")

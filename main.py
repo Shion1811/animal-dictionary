b46f403677a1cf7f220eb4be72b77479a9f6276d
shio <shion48691811@gmail.com>
2024-02-13 19:34:48 +0900

    静的ファイルのバグを修正

--- main.py
+++ main.py
@@ -22,5 +22,5 @@ def read_root():
     return {"page": "Search Page"}
 
 # 静的ファイルを設定
-app.mount("/", StatiFilesNoCache(directory="static",html=True), name="static")
 app.mount("/img-data", StatiFilesNoCache(directory="img-data",html=True), name="static")
+app.mount("/", StatiFilesNoCache(directory="static",html=True), name="static")

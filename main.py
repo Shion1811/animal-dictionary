27494bfc91bb251b3cda0ee4548f6878e25dea9d
shio <shion48691811@gmail.com>
2024-02-20 20:18:14 +0900

    microCMSの構成を試作

--- main.py
+++ main.py
@@ -3,15 +3,26 @@ from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 
+import os
+
+import requests
 from dotenv import load_dotenv
 
 from utils import StatiFilesNoCache
 
 load_dotenv()
+MICROCMS_API_KEY=os.environ.get("MICROCMS_API_KEY")
 
 app = FastAPI()
 
 templates = Jinja2Templates(directory="templates")
+
+@app.get("/words", response_class=HTMLResponse)
+async def read_words(request: Request):
+    res = requests.get('https://animal-dictionary.microcms.io/api/v1/words', headers={
+        "X-MICROCMS-API-KEY": MICROCMS_API_KEY
+    }, timeout=60)
+    return res.text
 
 @app.get("/words/{word}", response_class=HTMLResponse)
 async def read_word(request: Request, word: str):
